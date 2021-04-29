b7b4a16ddf6e82f19163b56bd37008df4ddf97f5
Abrar Shariar <shariarabrar@gmail.com>
2021-04-29 15:43:03 +0900

    Remove unnecessary styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,9 @@ import Routes from './routes';
 
 export const GlobalStyles = createGlobalStyle`
 * {
-  margin: 0;
-  padding: 0;
   outline: 0;
   box-sizing: border-box;
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  transition: 0.05s;
 }
 
 html,
