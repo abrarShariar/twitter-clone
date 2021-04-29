078704d6fa81b91baa151b82910dc1853dce5557
Abrar Shariar <shariarabrar@gmail.com>
2021-04-29 15:48:55 +0900

    Clean up unnecessary code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,6 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
 import Routes from './routes';
-
-export const GlobalStyles = createGlobalStyle`
-* {
-  outline: 0;
-  box-sizing: border-box;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-}
-
-html,
-body,
-#root {
-  height: 100%;
-}
-
-body {
-  background: #fff;
-}
-`;
+import { GlobalStyles } from './global-styles.js';
 
 function App() {
     return (
--- frontend/src/global-styles.js
+++ frontend/src/global-styles.js
@@ -1,6 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
 export const blue = '#1da1f2';
 export const darkBlue = '#1987c6';
 export const lightBlue = 'rgba(29, 161, 242, 0.1)';
 export const grey = '#E6ECF0';
 export const lightGrey = '#f7f7f7';
 export const darkGrey = '#989da0';
+
+export const GlobalStyles = createGlobalStyle`
+* {
+  outline: 0;
+  box-sizing: border-box;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+html,
+body,
+#root {
+  height: 100%;
+}
+
+body {
+  background: #fff;
+}
+`;
